e4e29d3edace7d234e3330af9abcb858408f4b71
songjaehoon <sjh101111@nate.com>
2024-02-16 17:55:40 +0900

    feat 4주차 weeklyQuiz파일 week4패키지에 추가
    1. 4주차 위클리 퀴즈 풀이 파일 추가

---
+++ src/quizPackage/weeklyQuiz/week4/StackAlgorithm.java
@@ -0,0 +1,34 @@
+package quizPackage.weeklyQuiz.week4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class StackAlgorithm {
+    public static void main(String[] args) {
+        Stack<String> stringStack = new Stack<>();
+        List<String> stringList = new ArrayList<>();
+        stringList.add("(");
+        stringList.add("(");
+        stringList.add("(");
+        stringList.add(")");
+        stringList.add(")");
+        stringList.add(")");
+        stringList.add("(");
+        for (String list : stringList) {
+            if (list.equals("(")) {
+                stringStack.push(list);
+            } else if (list.equals(")") && stringStack.isEmpty()) {
+                System.out.println("순서 혹은 짝이 맞지 않습니다.");
+                System.exit(1);
+                return;
+            } else if (list.equals(")") && !stringStack.isEmpty()) {
+                stringStack.pop();
+            }
+            } if (!stringStack.isEmpty()) {
+                System.out.println("짝이 맞지 않습니다.");
+            } else if (stringStack.isEmpty()) {
+                System.out.println("짝이 맞습니다.");
+        }
+    }
+}
